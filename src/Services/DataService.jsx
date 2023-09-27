412daacda40020ca430de7924d1686fb55ddf72b
aayala4569 <angelicaayala100@gmail.com>
2023-09-26 18:03:49 -0700

    added to data service

--- src/Services/DataService.jsx
+++ src/Services/DataService.jsx
@@ -11,4 +11,31 @@ function checkToken()
     return result;
 }
 
-export {checkToken}
+//We are going to use async and await function
+//help us resolve a promise
+//has to do with fetching
+//This can be named whatever (createdAccount)
+const createAccount = async (createdUser) =>
+{
+   let result = await fetch("http://localhost:5101/user/addusers", {
+        method: "POST",
+        //headers is an objects with a key value
+        headers: {
+            "Content-Type" : 'application/json'
+        },
+        //In parenthesis, has to be named the same as above
+        body: JSON.stringify(createdUser)
+    });
+
+    if(!result.ok)
+    {
+        const message = `An error has occured! ${result.status}`
+        throw new Error(message)
+    }
+    let data = await result.json();
+
+    console.log(data)
+
+}
+
+export {checkToken, createAccount}
--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import {Container, Row, Col, Button, Form} from 'react-bootstrap';
+import { createAccount } from '../Services/DataService';
 
 
 const CreateAccount = () => {
@@ -14,7 +15,8 @@ const CreateAccount = () => {
             username: Username,
             password: Password
         }
-        console.log(userData);
+        createAccount(userData);
+        console.log(userData)
     };
   return (
     <div>
